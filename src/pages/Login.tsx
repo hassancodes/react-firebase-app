4697bb20b2d3ff5af5a575f745aa474ad56c5691
Muhammad hassan <hassanhanjra900@gmail.com>
2023-08-05 16:10:06 -0400

    feat : firebase singin

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,17 @@
+import {auth,provider} from "../config/firebase";
+import { signInWithPopup } from "firebase/auth";
+
+
 export const Login=()=>{
-    return (<h1>This is login</h1>)
+
+    const signInWithGoogle=async ()=>{
+        const result = await signInWithPopup(auth,provider);
+        console.log(result);
+    }
+    return (
+    <div>
+    <h1>Sign in using Google Auth</h1>
+    <button onClick={signInWithGoogle}>Sign in </button>
+    </div>
+    )
 }
