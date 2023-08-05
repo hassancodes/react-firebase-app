433bd3ad48b1e8c81fd318dca5bceb06bc63127e
Muhammad hassan <hassanhanjra900@gmail.com>
2023-08-05 15:48:04 -0400

    getting started with firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBq6CUDFZpgszguiPC-gMDhZEuLZ69lrl8",
+  authDomain: "react-firebase-app-343c4.firebaseapp.com",
+  projectId: "react-firebase-app-343c4",
+  storageBucket: "react-firebase-app-343c4.appspot.com",
+  messagingSenderId: "137980940952",
+  appId: "1:137980940952:web:f747efa1a86af147a076ed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
