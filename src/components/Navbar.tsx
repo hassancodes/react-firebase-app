8b20ec82f229f1e53b6449bc1abf9c4cfbcaf3c6
Muhammad hassan <hassanhanjra900@gmail.com>
2023-08-06 11:53:31 -0400

    done with basic firebase

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,22 @@
 import  {Link} from "react-router-dom";
+import {auth} from "../config/firebase";
+
+// used to manage the current user
+import {useAuthState} from "react-firebase-hooks/auth"
 
 export const Navbar= ()=>{
+    const [user] = useAuthState(auth);
     return (
+        <div>
         <div>
     <Link to="/">Home  </Link>
     <Link to="/login">Login</Link>
+    </div>
+
+    <div>
+        <p>{user?.displayName}</p>
+        <img src={user?.photoURL || ""}/>
+    </div>
     </div>
     )
 }
