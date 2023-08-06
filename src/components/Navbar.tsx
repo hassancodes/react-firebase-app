0068ac776fefa4a5ff46c6399a9a18c419b5fb37
Muhammad hassan <hassanhanjra900@gmail.com>
2023-08-06 12:21:04 -0400

    feat: firebase logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,20 +3,32 @@ import {auth} from "../config/firebase";
 
 // used to manage the current user
 import {useAuthState} from "react-firebase-hooks/auth"
+import {signOut} from "firebase/auth";
 
 export const Navbar= ()=>{
     const [user] = useAuthState(auth);
+    const sighUserOut=async ()=>{
+       await signOut(auth)
+    }
     return (
         <div>
         <div>
     <Link to="/">Home  </Link>
     <Link to="/login">Login</Link>
+    {user && <button onClick={sighUserOut}>LogOut</button>}
+    
     </div>
 
     <div>
+        {user &&
+        <>        
         <p>{user?.displayName}</p>
         <img src={user?.photoURL || ""}/>
+        {/* <button onClick={sighUserOut}>LogOut</button> */}
+        </>
+        }
     </div>
+
     </div>
     )
 }
