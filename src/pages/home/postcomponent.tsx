1e80b7cbebb22d7894ff0adb70290017624b577d
Muhammad hassan <hassanhanjra900@gmail.com>
2023-08-07 19:25:58 -0400

    adding like functionality

--- src/pages/home/postcomponent.tsx
+++ src/pages/home/postcomponent.tsx
@@ -1,5 +1,7 @@
 import {Post as IPost} from "./Home"
-
+import {addDoc,collection} from "firebase/firestore";
+import {auth, db} from "../../config/firebase"
+import { useAuthState } from "react-firebase-hooks/auth";
 // imported the interface
 interface Props{
 post:IPost;
@@ -7,6 +9,18 @@ post:IPost;
 export const PostComponent=(props:Props)=>{
 // learn more about this concept 
     const {post} = props;
+    const [user] = useAuthState(auth);
+    // adding the like functionality
+    const likeRef = collection(db,"likes")
+    
+    const addLike=async ()=>{
+        await addDoc(likeRef,{id:user?.uid, postId:post?.id})
+
+    }
+
+
+
+    
     return (<>
     
     <div>
@@ -18,7 +32,9 @@ export const PostComponent=(props:Props)=>{
         </div>
         <div className="footer">
             <p>@{post.username}</p>
-          <button>&#128077;</button>
+          <button onClick={addLike}>&#128077;</button>
+          <p>Likes : </p>
+
         </div>
     </div>
     </>)
