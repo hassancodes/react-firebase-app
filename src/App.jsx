996485dec3f65b5e22b49ff2fa78991c01d1c212
Muhammad hassan <hassanhanjra900@gmail.com>
2023-09-29 11:17:30 -0400

    update:routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,26 @@ import {Login} from "./pages/Login";
 import {Home} from "./pages/home/Home";
 import {Navbar} from "./components/Navbar";
 import {CreatePost} from "./pages/create-post/CreatePost";
+import {useAuthState} from "react-firebase-hooks/auth"
+import { auth } from './config/firebase';
+
 
 
 function App() {
+  const [user] = useAuthState(auth);
 
-
-return (
-
-<div class="navbar-div">
+  
+  return (
+    
+    <div class="navbar-div">
   <Router>
+    {/* const navigate = useNavigate(); */}
     <Navbar />
+    
     <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/login" element={<Login />}/>
-      <Route path="/CreatePost" element={<CreatePost />}/>
+      <Route path="/" element={user ? <Home /> : <Login />}/>
+      <Route path="/login" element={user ? <Home /> : <Login />}/>
+      <Route path="/CreatePost" element={user ? <CreatePost /> : <Login/>}/>
     </Routes>
   </Router>
 </div>
