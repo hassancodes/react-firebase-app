2b433de78cf5aaa15ee538060426466b43798dcb
Muhammad hassan <hassanhanjra900@gmail.com>
2023-09-29 22:45:56 -0400

    update:design

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -32,9 +32,9 @@ export const FORMHTML=(props)=>{
 
 					<div class="wrap-input100 validate-input" data-validate="Enter password">
 						<span class="btn-show-pass">
-							<i class="zmdi zmdi-eye"></i>
+							{/* <i class="zmdi zmdi-eye"></i> */}
 						</span>
-						<input class="input100" type="password" name="pass"/>
+						<input class="input100"  name="pass"/>
 						<span class="focus-input100" data-placeholder="Password"></span>
 					</div>
 
